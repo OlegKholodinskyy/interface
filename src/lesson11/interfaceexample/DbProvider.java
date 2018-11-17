b142300f8021c3191bc01531cc22dbdbdb062e1f
Oleg_hos <ooleguner@gmail.com>
2018-11-17 23:38:06 +0200

    HomeWork_commit_1

---
+++ src/lesson11/interfaceexample/DbProvider.java
@@ -0,0 +1,8 @@
+package lesson11.interfaceexample;
+
+public interface DbProvider {
+
+    void connectToDb();
+    void disconnectFromDb();
+    void encryptData();
+}
--- src/lesson11HomeWork/BookingComAPI.java
+++ src/lesson11HomeWork/BookingComAPI.java
@@ -1,5 +1,7 @@
 package lesson11HomeWork;
 
+import java.util.ArrayList;
+
 public class BookingComAPI implements API {
     Room[] rooms;
 
@@ -9,11 +11,26 @@ public class BookingComAPI implements API {
 
     @Override
     public Room[] findRooms(int price, int persons, String city, String hotel) {
-        return new Room[0];
+        ArrayList<Room> resultSearch = new ArrayList<>();
+        for (Room room : rooms){
+            if (room!=null){
+                if (checkPrice(room.getPrice(), price) && room.getPersons() == persons || room.getCityName().equals(city) || room.getHotelName().equals(hotel)){
+                    resultSearch.add(room);
+                }
+            }
+        }
+        Room[] resultArray = new Room[resultSearch.size()];
+        return resultSearch.toArray(resultArray);
+    }
+    private boolean checkPrice(int priceRoom , int priceChecked) {
+        if ((priceChecked == priceRoom) || (priceChecked <= priceRoom +100 && priceChecked >= priceRoom -100))
+            return true;
+        else
+            return false;
     }
 
     @Override
     public Room[] getAll() {
-        return new Room[0];
+        return rooms;
     }
 }
