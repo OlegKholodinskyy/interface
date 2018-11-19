f71d76a4da5fcc6fc41044a5f35c2ff78994bb83
Oleg_hos <ooleguner@gmail.com>
2018-11-20 01:20:41 +0200

    HomeWork_tmp

--- src/lesson17/Solution.java
+++ src/lesson17/Solution.java
@@ -3,24 +3,72 @@ package lesson17;
 public class Solution {
 
     public static void main(String[] args) {
-        String str1 = "Hello word I am robot";
-        String str2 = "Hello";
+        String str1 = "Hello word";
+        String str2 = " Hello ";
         String str3 = "";
 
-        System.out.println(countWords(str1));
-        System.out.println(countWords(str2));
+//        System.out.println(countWords(str1));
+//        System.out.println(countWords(str2));
         System.out.println(countWords(str3));
 
 
     }
 
     public static int countWords(String input) {
+
+        char[] chars = input.toCharArray();
+        String[] arraySeparatedWords;
         int res = 0;
-        String[] arraySeparatedWords = input.split(" ");
+
+        if (!(countingSpaces(chars) == -1)) {
+            arraySeparatedWords = new String[countingSpaces(chars) + 1];
+            makeArrayWords(chars, arraySeparatedWords);
+            res = countingSeparatedWords(arraySeparatedWords);
+            return res;
+        } else {
+            System.out.println(" empty ");
+        }
+        return 0;
+    }
+
+    private static void makeArrayWords(char[] chars, String[] arraySeparatedWords) {
+        String tmp = "";
+      //  System.out.println(arraySeparatedWords.length);
+        int count = 0;
+        for (int i = 0; i < chars.length; i++) {
+            if (!(chars[i] == ' ')) {
+                tmp += chars[i];
+            }
+            else {
+                arraySeparatedWords[count] = tmp;
+                count++;
+                tmp = "";
+            }
+        }
+        arraySeparatedWords[count] = tmp;
+    }
+
+
+
+    public static int countingSeparatedWords(String[] arraySeparatedWords) {
+        int res = 0;
         for (String word : arraySeparatedWords) {
             if (!word.isEmpty())
                 res++;
         }
         return res;
+    }
+
+    public static int countingSpaces(char[] chars) {
+        if (!chars.equals(null)) {
+            int countOfSpace = 0;
+            for (char i : chars) {
+                if (i == ' ') {
+                    countOfSpace++;
+                }
+            }
+            return countOfSpace;
+        }
+        return -1;
     }
 }
