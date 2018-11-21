d4775d751e225f8021d34fbcdf2bcf814e5603a8
Oleg_hos <ooleguner@gmail.com>
2018-11-21 15:54:45 +0200

    HomeWork_commit_1

--- src/lesson17LongShort/Solution.java
+++ src/lesson17LongShort/Solution.java
@@ -13,7 +13,7 @@ public class Solution {
             int minSize = arrString[0].length();
             int index = 0;
             for (int i = 1; i < arrString.length; i++) {
-                if (checkLetter(arrString[i])) {
+                if (checkLetter(arrString[i])&& arrString[i].length()!=0) {
                     if (arrString[i].length() < minSize) {
                         minSize = arrString[i].length();
                         index = i;
@@ -36,7 +36,7 @@ public class Solution {
             int maxSize = arrString[0].length();
             int index = 0;
             for (int i = 1; i < arrString.length; i++) {
-                if (checkLetter(arrString[i])) {
+                if (checkLetter(arrString[i])&&arrString[i].length()!=0) {
                     if (arrString[i].length() > maxSize) {
                         maxSize = arrString[i].length();
                         index = i;
