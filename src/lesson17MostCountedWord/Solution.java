762896a443c4db86140d4a1634ce3fb8d16212b7
Oleg_hos <ooleguner@gmail.com>
2018-11-21 22:38:06 +0200

    HomeWork_commit_1

--- src/lesson17MostCountedWord/Solution.java
+++ src/lesson17MostCountedWord/Solution.java
@@ -10,25 +10,27 @@ public class Solution {
             int maxCount = 0;
             int index = 0;
             String[] arr = input.split(" ");
+            boolean fl=false;
 
             for (int i = 0; i < arr.length; i++) {
                 if (checkLetter(arr[i])) {
-count=0;
+                    count = 0;
                     for (int k = arr.length - 1; k >= 0; k--) {
                         if (checkLetter(arr[k]) && arr[k].equals(arr[i])) {
                             count++;
+                            fl = true;
                         }
                     }
                     if (maxCount < count) {
                         maxCount = count;
                         index = i;
                     }
-                    }
-                 else {
+                } else {
                     continue;
                 }
             }
-            return arr[index];
+            if (fl) return arr[index];
+            else return null;
         } else {
             return null;
         }
@@ -41,7 +43,7 @@ count=0;
 
     public static void main(String[] args) {
         String str = "The history ";
-        String str2 = "@456hh jkll @456hh";
+        String str2 = "@456hh @456hh";
         String str3 = "The history is ";
         String str4 = "one one first is 5 that a the first first";
         System.out.printf("in \" %-30s \" the most counted word is : %s %n", str, mostCountedWord(str));
