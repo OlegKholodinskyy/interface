69e16be41bd508c8b65b196afb7378905ecccbb3
Oleg_hos <ooleguner@gmail.com>
2018-11-21 22:52:20 +0200

    HomeWork_commit_1

--- src/lesson17MostCountedWord/Solution.java
+++ src/lesson17MostCountedWord/Solution.java
@@ -13,10 +13,10 @@ public class Solution {
             boolean fl=false;
 
             for (int i = 0; i < arr.length; i++) {
-                if (checkLetter(arr[i])) {
+                if (checkLetter(arr[i])&& !arr[i].equals("")) {
                     count = 0;
                     for (int k = arr.length - 1; k >= 0; k--) {
-                        if (checkLetter(arr[k]) && arr[k].equals(arr[i])) {
+                        if (checkLetter(arr[k]) && !arr[k].equals("") && arr[k].equals(arr[i])) {
                             count++;
                             fl = true;
                         }
@@ -46,11 +46,18 @@ public class Solution {
         String str2 = "@456hh @456hh";
         String str3 = "The history is ";
         String str4 = "one one first is 5 that a the first first";
+        String str5 = "";
+        String str6 = null;
+        String str7 = "7 7 7 7 7 7 7";
+        String str8 = "         tr        tr";
         System.out.printf("in \" %-30s \" the most counted word is : %s %n", str, mostCountedWord(str));
         System.out.printf("in \" %-30s \" the most counted word is : %s %n", str2, mostCountedWord(str2));
         System.out.printf("in \" %-30s \" the most counted word is : %s %n", str3, mostCountedWord(str3));
         System.out.printf("in \" %-30s \" the most counted word is : %s %n", str4, mostCountedWord(str4));
-
+        System.out.printf("in \" %-30s \" the most counted word is : %s %n", str5, mostCountedWord(str5));
+        System.out.printf("in \" %-30s \" the most counted word is : %s %n", str6, mostCountedWord(str6));
+        System.out.printf("in \" %-30s \" the most counted word is : %s %n", str7, mostCountedWord(str7));
+        System.out.printf("in \" %s \" the most counted word is : %s %n", str8, mostCountedWord(str8));
     }
 
 
