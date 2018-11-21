093cff9281b7b49fe6f651470721fa5017ecc3a5
Oleg_hos <ooleguner@gmail.com>
2018-11-21 11:37:30 +0200

    HomeWork_commit_1

---
+++ src/lesson17MostCountedWord/Solution.java
@@ -0,0 +1,39 @@
+package lesson17MostCountedWord;
+
+public class Solution {
+    /**
+     * mostCountedWord(String input)
+     */
+    private static String mostCountedWord(String input) {
+        if (check(input)) {
+            int count = 0;
+            int maxCount=0;
+            int index = 0;
+            String[] arr = input.split(" ");
+
+            for (int i = 0; i < arr.length; i++) {
+                for (int k = arr.length-1 ; k >=i; k--) {
+                     if (arr[i].equals(arr[k])){
+                         count++;
+                     }
+                     if (maxCount<count) {
+                         maxCount = count;
+                         index=i;
+                     }
+                }
+            }
+            return arr[index];
+        } else {
+            return null;
+        }
+    }
+
+    private static boolean check(String input) {
+        return !(input == null || input.equals(null) || input.trim().isEmpty() || input.length() == 0);
+    }
+
+    public static void main(String[] args) {
+        String str = "The history first is that the first first";
+        System.out.printf("in \" %-30s \" the most counted word is : %s %n", str, mostCountedWord(str));
+    }
+}
