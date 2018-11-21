7d152028f21a7a3ff0f01ecb8735ee1d736ed529
Oleg_hos <ooleguner@gmail.com>
2018-11-21 13:06:28 +0200

    HomeWork_commit_1

---
+++ src/lesson17Validate/Solution.java
@@ -0,0 +1,81 @@
+package lesson17Validate;
+
+public class Solution {
+    private static boolean validate(String address) {
+        if (check(address)) {
+            if (    checkHttp(address) &&
+                    checkDomain(address) &&
+                    chechBody(address)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean chechBody(String address) {
+        char[] ch = address.toCharArray();
+        int startIndex = 0;
+        int endIndex = address.length() - 4;
+        if (ch[6] == '/' && ch[7] == '/') {
+            startIndex = 8;
+        } else {
+            startIndex = 7;
+        }
+        String str = address.substring(startIndex, endIndex);
+        if (checkLetterOrDigit(str)) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    private static boolean checkLetterOrDigit(String str) {
+        char[] arr = str.toCharArray();
+        boolean flag = true;
+        for(char c : arr){
+            if (!Character.isLetter(c) && !Character.isDigit(c)) {
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
+
+    private static boolean checkDomain(String address) {
+
+        String str = address.substring((address.length() - 4), (address.length()));
+        if (str.equals(".com") || str.equals(".org") || str.equals(".net")) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    private static boolean checkHttp(String address) {
+        String str = address.substring(0, 7);
+        String str2 = address.substring(0, 8);
+
+        if (str2.equals("https://") || str.equals("http://")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private static boolean check(String input) {
+        return !(input == null || input.equals(null) || input.trim().isEmpty() || input.length() == 0);
+    }
+
+
+    public static void main(String[] args) {
+        String address = "http://gromco--de.com";
+        if (validate(address)){
+            System.out.println("true");
+        }
+        else {
+            System.out.println("false");
+        }
+    }
+}
