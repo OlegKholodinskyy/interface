4d21edfc09f9996f2aab54d80aaf1a41cfc7130f
Oleg_hos <ooleguner@gmail.com>
2018-12-05 11:01:00 +0200

    HomeWork_commit_1

--- src/lesson19/fileTransferException/Controller.java
+++ src/lesson19/fileTransferException/Controller.java
@@ -5,26 +5,26 @@ public class Controller {
     Validate validate = new Validate();
 
     public File put(Storage storage, File file) throws Exception {
-            validate.validateArgumentsPuttMethod(storage, file) ;
-            for (int i = 0; i < storage.getFiles().length; i++) {
-                if (storage.getFiles()[i] == null) {
-                    storage.getFiles()[i] = file;
-                    return file;
-                }
+        validate.validateArgumentsPuttMethod(storage, file);
+        for (int i = 0; i < storage.getFiles().length; i++) {
+            if (storage.getFiles()[i] == null) {
+                storage.getFiles()[i] = file;
+                return file;
             }
+        }
 
         return null;
     }
 
     public File delete(Storage storage, File file) throws Exception {
         validate.validateArgumentsDellMethod(storage, file);
-            for (int i = 0; i < storage.getFiles().length; i++) {
-                if (storage.getFiles()[i] != null && storage.getFiles()[i].getId() == file.getId()) {
-                    storage.getFiles()[i] = null;
-                    break;
-                }
+        for (int i = 0; i < storage.getFiles().length; i++) {
+            if (storage.getFiles()[i] != null && storage.getFiles()[i].getId() == file.getId()) {
+                storage.getFiles()[i] = null;
+                break;
             }
-            return file;
+        }
+        return file;
     }
 
     public void transferAll(Storage storageFrom, Storage storageTo) throws Exception {
@@ -32,27 +32,28 @@ public class Controller {
             if (fileStorageSource != null) {
                 validate.validateArgumentsDellMethod(storageFrom, fileStorageSource);
                 validate.validateArgumentsPuttMethod(storageTo, fileStorageSource);
-               // throw new Exception("All files from storage id : " + storageFrom.getId() + " can not transfer to storage is : " + storageTo.getId());
             }
         }
 
-        for (File fileStorageSource : storageFrom.getFiles()) {
-       //     long id = fileStorageSource.getId();
-            if (fileStorageSource != null) {
-                put(storageTo, fileStorageSource);
-                delete(storageFrom, fileStorageSource);
+        for (int j = 0; j < storageFrom.getFiles().length; j++) {
+            for (int i = 0; i < storageTo.getFiles().length; i++) {
+                if (storageTo.getFiles()[i] == null) {
+                    storageTo.getFiles()[i] = storageFrom.getFiles()[j];
+                    storageFrom.getFiles()[j] = null;
+                }
             }
         }
+
     }
 
     File transferFile(Storage storageFrom, Storage storageTo, long id) throws Exception {
 
         File founded = validate.getFileByID(id, storageFrom.getFiles());
-            validate.validateArgumentsDellMethod(storageFrom, founded);
-            validate.validateArgumentsPuttMethod(storageTo, founded);
-            put(storageTo, founded);
-            delete(storageFrom, founded);
-            return founded;
+        validate.validateArgumentsDellMethod(storageFrom, founded);
+        validate.validateArgumentsPuttMethod(storageTo, founded);
+        put(storageTo, founded);
+        delete(storageFrom, founded);
+        return founded;
     }
 
 }
