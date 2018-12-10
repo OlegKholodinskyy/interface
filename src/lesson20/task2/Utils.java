c0a0b285c1834ae5a424552b3d84b903eef7f917
Oleg_hos <ooleguner@gmail.com>
2018-12-10 16:47:53 +0200

    HomeWork_commit_1

--- src/lesson20/task2/Utils.java
+++ src/lesson20/task2/Utils.java
@@ -3,7 +3,7 @@ package lesson20.task2;
 public class Utils {
     private int limitTransactionsPerDayCount=10;
     private int limitTransactionsPerDayAmount=100;
-    private int limitSimpleTransactionAmount =10;
+    private int limitSimpleTransactionAmount =40;
     private String[] cities = {"Kiev", "Odessa"};
 
     public Utils() {
