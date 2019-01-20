ce5e4947fa94d38038f838b4ca397e8ea329a293
Oleg_hos <ooleguner@gmail.com>
2019-01-20 11:26:42 +0200

    HomeWork_commit_1

---
+++ src/lesson31/homeWork/Demo.java
@@ -0,0 +1,62 @@
+package lesson31.homeWork;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        try {
+            System.out.println(Solution.countSymbols(""));
+        } catch (BadRequestException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            System.out.println(Solution.countSymbols("245243 345"));
+        } catch (BadRequestException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            System.out.println(Solution.countSymbols("cdddddddddd   5  45 45trg r4 "));
+        } catch (BadRequestException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            System.out.println(Solution.countSymbols(null));
+        } catch (BadRequestException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            System.out.println(Solution.countSymbols("            "));
+        } catch (BadRequestException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+        try {
+            System.out.println(Solution.words(" get put get           "));
+        } catch (BadRequestException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            System.out.println(Solution.words("     "));
+        } catch (BadRequestException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            System.out.println(Solution.words(" g3et put3 g3et     tyry  null      "));
+        } catch (BadRequestException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            System.out.println(Solution.words( null));
+        } catch (BadRequestException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
